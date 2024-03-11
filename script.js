84a3fa2eb74712519afeb8d7e07abe0f0d8857ea
nitidoart <113176158+nitidoart@users.noreply.github.com>
2024-03-11 11:11:09 +0100

    modified(script.js):animacion elementos del doom

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-document.addEventListener("DOMContentLoaded", function() {
+let stars = document.getElementById('starts');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountains_front = document.getElementById('mountains_front');
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    /* stars.style.left = value * 0.25 + 'px'; */
+    /* stars.style.transform = 'rotate(' + value + 'deg)'; */
+    let rotation = value / (window.innerHeight / 90);
+    stars.style.transform = 'rotate(' + rotation + 'deg)';
+    moon.style.top = value * 1.05 + 'px';
+     /* mountains_behind.style.top = value * 1.5 + 'px'; */
+    /* mountains_front.style.top = value * -1.0 + 'px'; */
+    mountains_front.style.transform = 'scale(' + (1 + value / 3000) + ')';
+    text.style.marginRight = value * 4 + 'px';
+    /* text.style.marginTop = value * -1.5 + 'px'; */
+    btn.style.marginTop = value * 0.5 + 'px';
+    header.style.top = value * 0.5 + 'px';
+
+   
+     
+    /* mountains_front.style.zoom = value * -1.0 + 'px'; */
+})
+
+
+
+
+
+
+/* document.addEventListener("DOMContentLoaded", function() {
     // Cargamos el SVG utilizando fetch
     fetch("./assets/mountains.svg")
         .then(response => response.text())
@@ -11,4 +44,5 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("svgContainerStart").appendChild(startsLayer);
         })
         .catch(error => console.error("Error cargando el archivo SVG", error));
-});
+}); */
+
